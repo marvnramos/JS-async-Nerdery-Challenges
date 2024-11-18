01770b66571326598a9d7fd5f392ae079d4ea8ce
marvnramos <marvinramos@ravn.co>
2024-11-18 09:53:27 -0600

    feat(validateUser): implement logic to iterate through an array and group items into success and failure categories

--- 1-callbacks/solution.js
+++ 1-callbacks/solution.js
@@ -32,14 +32,44 @@ node solution.js name1 name2 name3
 ** give a look to node.js util.promisify, avoid to alter the validate-user.file **
 */
 
+const validateUser = require('./validate-user');
+
 function solution() {
     // YOUR SOLUTION GOES HERE
-
     // you get your 5 names here
+    const NAMES = ['John', 'Mary', 'Gabriel', 'Angie', 'Carlos'];
+    const success = [];
+    const failure = [];
 
     // iterate the names array and validate them with the method
+    const promises = NAMES.map(name => {
+        return new Promise(
+            async (resolve, reject) => {
+                try {
+                    validateUser(name, (err, data) => {
+                        if (err) {
+                            failure.push(err.message);
+                            resolve();
+                        } else {
+                            success.push(`id: ${data.id}\nname: ${data.name}`);
+                            resolve();
+                        }
+                    })
+                } catch (err) {
+                    reject(err);
+                }
+            }
+        );
+    });
 
     // log the final result
+    Promise.all(promises).then(() => {
+        console.log('Success\n');
+        success.forEach(succ => console.log(succ));
+
+        console.log('\nFailure\n');
+        failure.forEach(fail => console.log(fail));
+    });
 }
 
 solution()
